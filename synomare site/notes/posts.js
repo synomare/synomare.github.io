ac1a40186c9e2a2a53948d5ee8df91df61e32cc2
synomare <synomare@gmail.com>
2025-09-22 23:30:34 +0900

    Add files via upload

---
+++ synomare site/notes/posts.js
@@ -0,0 +1,46 @@
+(function(){
+  window.__SYNOMARE_POSTS__ = [
+  {
+<<<<<<< HEAD
+    "slug": "sample",
+    "title": "タグ対応テスト",
+    "date": "2025-09-18",
+    "summary": "ここに記事の概要を1〜2文で書いてください。",
+    "tags": [
+      "test",
+      "log"
+    ],
+    "href": "sample.html",
+    "year": "2025",
+    "yearMonth": "2025-09",
+    "path": "notes/sample.html"
+  },
+  {
+    "slug": "hello",
+    "title": "最初の投稿",
+    "date": "2025-09-16",
+    "summary": "ここに本文を書いてください。複数段落でもOKです。",
+    "tags": [
+      "intro",
+      "update"
+    ],
+    "href": "hello.html",
+    "year": "2025",
+    "yearMonth": "2025-09",
+    "path": "notes/hello.html"
+=======
+    "slug": "test",
+    "title": "サイト開設",
+    "date": "2025-09-22",
+    "summary": "テスト投稿",
+    "tags": [
+      "test"
+    ],
+    "href": "test.html",
+    "year": "2025",
+    "yearMonth": "2025-09",
+    "path": "notes/test.html"
+>>>>>>> 75ff897 (Initial commit)
+  }
+];
+})();
