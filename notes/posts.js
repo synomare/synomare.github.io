75ff897b32baa1294220587d085c0e3f85bdc789
synomare <synomare@gmail.com>
2025-09-22 15:45:51 +0900

    Initial commit

---
+++ notes/posts.js
@@ -0,0 +1,17 @@
+(function(){
+  window.__SYNOMARE_POSTS__ = [
+  {
+    "slug": "test",
+    "title": "サイト開設",
+    "date": "2025-09-22",
+    "summary": "テスト投稿",
+    "tags": [
+      "test"
+    ],
+    "href": "test.html",
+    "year": "2025",
+    "yearMonth": "2025-09",
+    "path": "notes/test.html"
+  }
+];
+})();
